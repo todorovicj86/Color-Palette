031e6212e4b5953c40de53e156ba1a3e8e60689a
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-09-26 15:08:58 +0200

    Add PaletteShades Component
    A component to create and display different shades of one color.

---
+++ src/PaletteShades.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PaletteNavbar from './PaletteNavbar'
+import ColorBox from './ColorBox'
+import Button from '@material-ui/core/Button';
+// import Grid from '@material-ui/core/Grid'
+import chroma from 'chroma-js'
+import './PaletteShades.css'
+import uuid from 'uuid'
+
+
+class PaletteShades extends Component {
+//  make shades of one color
+    getShades(){
+        let shadeObj =[]
+        const baseColor = this.props.color.color;
+        const name = this.props.color.name;
+        const darkestShade = chroma(baseColor).darken(4);
+        const brightestShade = chroma(baseColor).brighten(2.6)
+        const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+        let colorShade = chroma.scale([brightestShade, baseColor, darkestShade]).colors(10); 
+        for(let i = 0; i < levels.length; i++){
+            let data = {
+                name: name + " " + levels[i],
+                color: colorShade[i]
+            }
+            shadeObj.push(data)
+        }
+        return shadeObj;
+    }
+
+    render(){
+        const shades = this.getShades();
+       
+        const colorShades = shades.map(bgColor => (
+            <ColorBox 
+                colorName = {bgColor.name} 
+                bgColor = {
+                    (this.props.format === 'hex' &&  chroma(bgColor.color).hex()) ||
+                    (this.props.format === "rgb" && chroma(bgColor.color).css()) ||
+                    (this.props.format === "rgba" && chroma(bgColor.color).alpha(0.9).css())                                                             
+                }
+                onCopy = {this.props.onCopy}
+                format = {this.props.format}
+                key = {uuid()}                                               
+            />
+        
+        ))
+        return(
+            <div className="PaletteShades">
+            <div className="PaletteShades-header">
+                <PaletteNavbar handleFormat = {this.props.handleFormat} format = {this.props.format}/>
+                <div className = "PaletteShades-link">
+                    <Button variant="contained" color="primary">
+                        <Link className= "LinkBackTo" to = {`/palette/${this.props.palette.id}`}>Go back</Link>
+                    </Button>
+                </div>
+            </div>
+                <div className="ShadeBoxes">
+                    {colorShades}
+                </div>     
+            </div>
+        )
+    }
+}
+
+export default PaletteShades;
