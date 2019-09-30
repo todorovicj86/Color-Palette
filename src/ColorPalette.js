1b88a35b77e1b336a53727fe53f8f32e806a33d0
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-09-30 11:33:15 +0200

    Update ColorPalette.js

--- src/ColorPalette.js
+++ src/ColorPalette.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-// import {CopyToClipboard} from 'react-copy-to-clipboard'
-// import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import Button from '@material-ui/core/Button';
 import ColorBox from './ColorBox'
 import PaletteFooter from './PaletteFooter'
 import PaletteNavbar from './PaletteNavbar'
-// import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import chroma from 'chroma-js'
-import './ColorPalette.css'
 import {getPaletteShades} from './helpers'
+import './ColorPalette.css'
 import uuid from 'uuid'
 
 class ColorPalette extends Component {
@@ -17,6 +13,7 @@ class ColorPalette extends Component {
         super(props);
         this.state = {
             shadeLevel: 500,
+
         }
         this.handleCopy = this.handleCopy.bind(this)
         this.changeShade = this.changeShade.bind(this)
@@ -31,41 +28,35 @@ class ColorPalette extends Component {
             shadeLevel: shadeLevel
         })
     }
+
     render(){
 
-        const palette = this.props.palette;
+        const {palette, format, sliderMarks,handleFormat} = this.props;
         const colorBoxes = palette.colors[this.state.shadeLevel].map (color => (
             <ColorBox 
-                    bgColor = {
-                        (this.props.format === 'hex' &&  chroma(color.hex).hex()) ||
-                        (this.props.format === "rgb" && chroma(color.rgb).css()) ||
-                        (this.props.format === "rgba" && chroma(color.rgba).alpha(0.9).css())                                                             
-                    }
+                    bgColor = {color[format]}
                     colorName = {color.name}
                     key={uuid()}
                     onCopy = {this.handleCopy}
-                    format = {this.props.format}
+                    format = {format}
                     paletteId = {palette.id}
             />
         ))
-        // console.log(palette.colors[0])
-        // for(let i = 0; i < palette.colors.length; i++){
-        //     console.log(palette.colors[i].color)
-        //   }
           
           getPaletteShades(palette)
         return (
             <div className="ColorPalette">
                 <div className="ColorPalette-header">
-                    <PaletteNavbar handleFormat = {this.props.handleFormat} 
-                                    format = {this.props.format} 
-                                    marks = {this.props.sliderMarks}
-                                    changeShade = {this.changeShade}
-                                    />
+                    <PaletteNavbar 
+                        handleFormat = {handleFormat} 
+                        format = {format} 
+                        marks = {sliderMarks}
+                        changeShade = {this.changeShade}
+                    />
 
                     <div className="ColorPalette-link">
                          <Button>
-                            <Link className= "LinkBackTo" id="LinkBackTo" to = "/"><i class="fas fa-arrow-left"></i> Go back</Link>
+                            <Link className= "LinkBackTo" id="LinkBackTo" to = "/"><i className="fas fa-arrow-left"></i> Go back</Link>
                         </Button>
                     </div>
                 </div>
