8583537835e0afdb935f119222e35b9981e52431
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-08 12:44:55 +0200

    Remove color from the first palette

--- src/seedColors.js
+++ src/seedColors.js
@@ -23,7 +23,7 @@ export default [
         { name: "brown", color: "#795548" },
         { name: "grey", color: "#9E9E9E" },
         { name: "bluegrey", color: "#607D8B" },
-        { name: "Asbestos", color: "#7f8c8d" }
+        
       
       ]
     },
