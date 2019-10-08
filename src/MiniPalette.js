488abdca2770a01ea0682af74e85f43775d61776
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-08 14:49:59 +0200

    Change to class and add delete button

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/MiniPaletteStyles'
+import IconButton from '@material-ui/core/IconButton';
 
 
 
-function MiniPalette (props) {
-    const {classes, id, paletteName, colors, emoji} = props;
+class MiniPalette extends Component {
+    constructor(props){
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this)
+    }
 
-    return(
-        <div className={classes.root} onClick={props.handleClick}>
-            <div className={classes.colors}>
-                {colors.map(color => (
-                    <div key={color.name} 
-                        className={classes.color} style={{backgroundColor: color.color}}
-                    ></div>
-                ))}
+    handleDelete(evt){
+        const id = evt.target.id;
+        console.log(id)
+        this.props.handleDelete(id)
+    }
+    render(){
+    const {classes, id, paletteName, colors, emoji, handleClick} = this.props;
+
+        return(
+            <div className={classes.root} >
+                <div className={classes.colors}>
+                    {colors.map(color => (
+                        <div key={color.name} 
+                            className={classes.color} style={{backgroundColor: color.color}}
+                        ></div>
+                    ))}
+                </div>
+                <h5 className={classes.title} onClick={handleClick}>
+                    {paletteName}
+                    <span className={classes.emoji}>{emoji}</span>
+                </h5> 
+                <IconButton  onClick={this.handleDelete} className ={classes.delete} id={id}>
+                    <i id={id} className ="fas fa-trash"></i>
+                </IconButton>            
+                
             </div>
-            <h5 className={classes.title}>
-                {paletteName}
-                <span className={classes.emoji}>{emoji}</span>
-            </h5>              
-            
-        </div>
-    )
+        )
+    }
 }
 
 export default withStyles(styles)(MiniPalette);
