2f6d3e3bd20187369a25df7f7ef4931a634527fc
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-09 17:53:33 +0200

    Responsive ColorPaletteList - add media query

--- src/styles/ColorPaletteListStyles.js
+++ src/styles/ColorPaletteListStyles.js
@@ -1,10 +1,22 @@
-export default {
+import mediaquery from './mediaquery'
+
+const styles = {
+    "@global": {
+        ".fade-exit": {
+            opacity: 1,
+        },
+        
+        ".fade-exit-active": {
+            opacity: 0,
+            transition: "opacity 500ms ease-out",
+        }
+    },
     root: {
         alignItems: "flex-start",
-        backgroundColor: "blue",
+        // backgroundColor: "blue",
         color: "white",
         display: "flex",
-        // flexWrap: 'wrap',
+        flexWrap: 'wrap',
         justifyContent: "center",
         height: "100vh",
     },
@@ -13,7 +25,19 @@ export default {
         display: "flex",
         flexDirection: 'column',
         flexWrap: 'wrap',
-        width: "60%",
+        width: "50%",
+        [mediaquery.down("xl")]: {
+            width: "60%",
+        },
+        [mediaquery.down("lg")]: {
+            width: "70%",
+        },
+        [mediaquery.down("md")]: {
+            width: "80%",
+        },
+        [mediaquery.down("xs")]: {
+            width: "70%",
+        }
     },
     navbar: {
         alignItems: "center",
@@ -23,12 +47,27 @@ export default {
         width: "100%",
     },
     title: {
+        [mediaquery.down("md")]: {
+            fontSize: "2rem",
+        },
+        [mediaquery.down("xs")]: {
+            fontSize: "1.2rem",
+        },
+
  
     },
     link: {
         "& a" :{
             color: "white",
-        }
+        },
+        [mediaquery.down("md")]: {
+            textAlign: "right",
+            fontSize: "1.5rem"
+        },
+        [mediaquery.down("xs")]: {
+            fontSize: "0.8rem",
+        },
+   
 
     },
 
@@ -37,7 +76,18 @@ export default {
         width:"100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%",
+        gridGap: "2rem",
+        [mediaquery.down("md")]: {
+            gridTemplateColumns: "repeat(2, 50%)",
+            gridGap: "1.5rem",
+        },
+        [mediaquery.down("xs")]: {
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: "1.5rem",
+        }
     },
 
 }
+
+
+export default  styles;
