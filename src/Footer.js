1578d1c5c8769253738104601665a1658eddacea
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-09 18:53:31 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import styles from './styles/FooterStyles'
 
 
 function Footer(props){
-    const {classes, paletteName, emoji, showingFullPalette} = props;
+    const {classes, paletteName, emoji} = props;
     return(
         <footer className={classes.root}>
             {paletteName}
