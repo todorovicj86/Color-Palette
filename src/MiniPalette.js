a3d1a3daf619a8fc9c9f2b8ba851d1eabea77a50
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-09 18:53:49 +0200

    Update MiniPalette.js

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -10,24 +10,25 @@ class MiniPalette extends Component {
 
         return(
             <div className={classes.root} onClick={this.props.handleClick}>
-                <div className={classes.colors}>
-                    {colors.map(color => (
-                        <div key={color.name} 
-                            className={classes.color} style={{backgroundColor: color.color}}
-                        ></div>
-                    ))}
-                </div>
-                <h5 className={classes.title} onClick={handleClick}>
-                    {paletteName}
-                    <span className={classes.emoji}>{emoji}</span>
-                </h5> 
-                <IconButton className ={classes.delete} id={id}  
-                    onClick= {this.props.openDeleteDialog} 
-                    
-                >
-                    <i id={id} className ="fas fa-trash"></i>
-                </IconButton> 
-                    
+              
+                    <div className={classes.colors}>
+                        {colors.map(color => (
+                            <div key={color.name} 
+                                className={classes.color} style={{backgroundColor: color.color}}
+                            ></div>
+                        ))}
+                    </div>
+                    <h5 className={classes.title} onClick={handleClick}>
+                        {paletteName}
+                        <span className={classes.emoji}>{emoji}</span>
+                    </h5> 
+                    <IconButton className ={classes.delete} id={id}  
+                        onClick= {this.props.openDeleteDialog} 
+                        
+                    >
+                        <i id={id} className ="fas fa-trash"></i>
+                    </IconButton> 
+                
                 
             </div>
         )
